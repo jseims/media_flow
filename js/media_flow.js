b4425b86d2ec072410008c968cc023f8e70a6362
Joshua Seims <jseims@hotmail.com>
2012-09-18 00:56:01 +0000

    plugin version

--- js/media_flow.js
+++ js/media_flow.js
@@ -1,270 +1,294 @@
-var media_flow = media_flow || {};
+(function($) {
 
-var $top;
-var $div_array;
-var _frame_x;
-var _frame_y;
+    // really, safari?
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function (bind) {
+            var self = this;
+            return function () {
+                var args = Array.prototype.slice.call(arguments);
+                return self.apply(bind || null, args);
+            };
+        };    
+    }
+    
+    var methods = {
+        init : function( options ) { 
+            return this.each(function() {
+                var $this = $(this);
+                var opts = $.extend({}, $.fn.media_flow.defaults, options);
 
-var _rows = 0;
-var _cols = 0;
-var _width = 0;
-var _height = 0;
-var _totalWidth = 0;
-var _compressLayout = false;
-
-var _opts;
-var _defaults = {
-    padding : 0,
-    clipWidth : 0,
-    clipHeight : 0,
-    clipLeft : 0,
-    clipTop : 0,
-    offsetLeft : 0,
-    offsetTop : 0,
-    speed : 3,
-    render : function() { console.log("onRender undefined"); },
-    getData : function() { console.log("onGetData undefined"); },
-    onClick : function() { console.log("onClick undefined"); },
-    idle : function() { },
-    decorateCell : null,
-};
-
-var _padding = 0;
-var _clipWidth = 0;
-var _clipHeight = 0;
-var _clipLeft = 0;
-var _clipTop = 0;
-var _offsetLeft = 0;
-var _offsetTop = 0;
-
-var _started = false;
-var _curFrame = 0;
-var _intervalId;
-
-media_flow.setGridDimensions = function(rows, cols, width, height, options) {
-    _rows = rows;
-    _cols = cols;
-    _width = width;
-    _height = height;
-    _compressLayout = false;
-    _opts = $.extend({}, _defaults, options);
-}
-
-media_flow.setLiquidDimensions = function(rows, totalWidth, height, options) {
-    _rows = rows;
-    _totalWidth = totalWidth;
-    _height = height;
-    _compressLayout = true;
-    _opts = $.extend({}, _defaults, options);
-}
-
-media_flow.start = function(parent_div) {
-    if (!_started) {
-        _started = true;
-        $top = $('#' + parent_div);
-        _frame_x = $top.offset().left;
-        _frame_y = $top.offset().top;
+                if (opts.layout === "grid") {
+                    opts.compressLayout = false;
+                }
+                
+                var state = new flowState(opts);
+                
+                $this.data("flow_state", state);
+            });
+        },
         
-        if (_opts.clipWidth == 0) {
-            if (_compressLayout) {
-                _opts.clipWidth = _totalWidth;
-            } else  {
-                _opts.clipWidth = 2 * _opts.padding + (_cols - 1) * (_width + _opts.padding);
+        start : function( ) {
+            var $this = $(this);
+            var state = $this.data("flow_state");
+            if (!state.started) {
+                state.started = true;
+                state.frame_x = $this.offset().left;
+                state.frame_y = $this.offset().top;
+                state.$div_array = new Array(state.opts.rows)
+                
+                if (state.opts.clipWidth == 0) {
+                    if (state.opts.compressLayout) {
+                        state.opts.clipWidth = state.opts.totalWidth;
+                    } else  {
+                        state.opts.clipWidth = 2 * state.opts.padding + (state.opts.cols - 1) * (state.opts.width + state.opts.padding);
+                    }
+                    state.opts.clipHeight = state.opts.padding + state.opts.rows * (state.opts.height + state.opts.padding);
+                }
+                
+                // need to insert absolute positioned div for clipping to work
+                var $temp = $("<div style='position: absolute; left: " + (state.frame_x + state.opts.offsetLeft) + "px; clip: rect(" + 0 + "px, " + state.opts.clipWidth + "px, " + state.opts.clipHeight + "px, " + 0 + "px);'>");
+                $this.append($temp);
+                state.$top = $temp;
+                
+                for(var row = 0; row < state.opts.rows; row++) {
+                    state.$div_array[row] = new Array();        
+                }
+                
+                methods.clear.apply(this);
             }
-            _opts.clipHeight = _opts.padding + _rows * (_height + _opts.padding);
-        }
+        },
         
-        // need to insert absolute positioned div for clipping to work
-        
-        // commenting out top position as it's giving weird effects on whimsy
-        //var $temp = $("<div style='position: absolute; top: " + (_frame_y + _offsetTop) + "px; left: " + (_frame_x + _offsetLeft) + "px; clip: rect(" + 0 + "px, " + _clipWidth + "px, " + _clipHeight + "px, " + 0 + "px);'>");
-
-        var $temp = $("<div style='position: absolute; left: " + (_frame_x + _opts.offsetLeft) + "px; clip: rect(" + 0 + "px, " + _opts.clipWidth + "px, " + _opts.clipHeight + "px, " + 0 + "px);'>");
-        $top.append($temp);
-        $top = $temp;
-        
-        //console.log("x = " + _frame_x + " y = " + _frame_y);
-        $div_array = new Array(_rows)
-        for(var row = 0; row < _rows; row++) {
-            $div_array[row] = new Array();        
-        }
-        
-        media_flow.clear();
-        
-        _intervalId = setInterval(media_flow.frameStep, 30);
-    }
-}
-
-media_flow.clear = function() {
-    // clear out old values
-    for(var row = 0; row < _rows; row++) {
-        var rows = $div_array[row];
-        while(true) {
-            var $cell = rows.pop();
-            if ($cell == null) {
-                break;
+        setSpeed : function(speed) {
+            var $this = $(this);
+            var state = $this.data("flow_state");
+            var curPaused = state.paused && state.opts.speed != 0;
+            state.opts.speed = speed;
+            // pause and resume to apply new speed to animations
+            methods.pause.apply(this);
+            if (speed > 0 && curPaused === false) {
+                methods.resume.apply(this);
             }
-            $cell.remove();
-        }
-    }
-    
-    // now render new ones
-    for(var row = 0; row < _rows; row++) {
-        var rows = $div_array[row];
-        for(var col = 0; !media_flow.rowFull(row) && col < 100; col++) {
-            media_flow.render(row);
-        }
-    }
-}
+        },
 
-media_flow.rowFull = function(row) {
-    if (_compressLayout) {
-        var lastX = 0;
-        if ($div_array[row].length > 0) {
-            var $cell = $div_array[row][$div_array[row].length - 1];
-            lastX = $cell.offset().left + $cell.width();
-        }
-        return lastX > _totalWidth;
-    } else {
-        return $div_array[row].length >= _cols;
-    }
-}
+        // apply a function to all cells
+        map : function(func) {
+            var $this = $(this);
+            var state = $this.data("flow_state");
+            state.map(func);
+        },
 
+        pause : function() {
+            var $this = $(this);
+            var state = $this.data("flow_state");
+            if (state.started && state.paused === false) {
+                state.paused = true;
+                state.pauseCells();
+            }
+        },
 
-media_flow.render = function(row) {
-    var data = _opts.getData();
-    
-    if (data != null) {
-        html = _opts.render(data);
-        //console.log(data);
-        //console.log("html for row " + row + " is " + html);
-        var width = _width;
-        if (_compressLayout) {
-            width = data.width;
-        }
-        var $cell = $("<div style='position: absolute; width: " + width + "px;'>" + html + "</div>");
+        resume : function() {
+            var $this = $(this);
+            var state = $this.data("flow_state");
+            if (state.started && state.paused === true) {
+                state.curFrame = 0;
+                state.paused = false;
+                state.animateCells();
+            }
+        },
         
-        if (_opts.decorateCell != null) {
-            _opts.decorateCell($cell, data);
-        }
-        
-        $top.append($cell);
-        
-        $cell.click(media_flow.makeClickCallback(data));
-        
-        media_flow.positionCell($cell, row);
-        $div_array[row].push($cell);
-    }
-}
-
-media_flow.positionCell = function($cell, row) {
-    var x = 0;
-    if ($div_array[row].length > 0) {
-        var $lastCell = $div_array[row][$div_array[row].length - 1];
-        x = $lastCell.offset().left + $lastCell.width();
-        //console.log("position cell, num cells " + $div_array[row].length + " last one's left " + $lastCell.offset().left + " width " + $lastCell.width());
-    }
-    x += _opts.padding - _frame_x - _opts.offsetLeft;
-    
-    //var x = 2 * _opts.padding + col * (_width + _opts.padding) - offset;
-    var y = _opts.padding + row * (_height + _opts.padding);
-    $cell.css( { "left" : x + "px", "top" : y + "px" } );
-    //console.log("set x " + x + " frame_x " + _frame_x);
-}
-
-media_flow.scrolledOff = function(row) {
-    if ($div_array[row].length > 0) {
-        var $cell = $div_array[row][0];
-        var scroll = _opts.padding - ($cell.offset().left - _frame_x - _opts.offsetLeft + $cell.width());
-        //console.log("scroll " + scroll);
-        return scroll > 0;
-    } else {
-        return false;
-    }
-}
-
-media_flow.moveLeft = function($cell) {
-   // err, I don't know why but every time I set the $cell.offset.left, it would move right by the
-   // same number of pixels left that the containing div was (if containing div was also aboslutely positioned)
-   // so I substract that distance each time
-    $cell.css( { "left" : ($cell.offset().left - _opts.speed - _frame_x - _opts.offsetLeft) + "px" } );
-    
-}
-
-media_flow.setSpeed = function(speed) {
-    _opts.speed = speed;
-    if (speed === 0) {
-        media_flow.pause();
-    } else {
-        media_flow.resume();
-    }
-}
-
-media_flow.frameStep = function() {
-    _curFrame++;
-    // stop playing if no interaction for a while
-    if (_curFrame > 30 * 5 * 60) {
-        _opts.idle();
-        return;
-    }
-    
-    // move everyone left
-    media_flow.map(media_flow.moveLeft);
-
-    // check if we need to throw away old thumbs, load new ones
-    for(var row = 0; row < _rows; row++) {
-        if (media_flow.scrolledOff(row)) {
-            // delete first one
-            $div_array[row][0].remove();
+        clear : function() {
+            var $this = $(this);
+            var state = $this.data("flow_state");
             
-            // compress div_array
-            $div_array[row].shift();        
-        }
-    
-        if (!media_flow.rowFull(row)) {
-            // add a new object
-            media_flow.render(row);
-        }
-    }    
-}
-
-// apply a function to all cells
-media_flow.map = function(func) {
-    if (_started) {
-        for(var row = 0; row < _rows; row++) {
-            for(var col = 0; col < $div_array[row].length; col++) {
-                func($div_array[row][col]);
+            if (state.started) {
+                // clear out old values
+                for(var row = 0; row < state.opts.rows; row++) {
+                    var rows = state.$div_array[row];
+                    while(true) {
+                        var $cell = rows.pop();
+                        if ($cell == null) {
+                            break;
+                        }
+                        $cell.stop();
+                        $cell.remove();
+                    }
+                }
+                
+                // now render new ones
+                for(var row = 0; row < state.opts.rows; row++) {
+                    var rows = state.$div_array[row];
+                    for(var col = 0; !state.rowFull(row) && col < 100; col++) {
+                        state.render(row);
+                    }
+                }
             }
-        }
-    }
-}
-
-media_flow.idle = function() {
-    clearInterval(_intervalId);
-    _idle();
-}
-
-media_flow.pause = function() {
-    if (_started) {
-        clearInterval(_intervalId);
-        _intervalId = -1;
-    }
-}
-
-media_flow.resume = function() {
-    if (_started) {
-        _curFrame = 0;
-        if (_intervalId == -1) {
-            _intervalId = setInterval(media_flow.frameStep, 30);
-        }
-    }
-}
-
-// hack to deal with JS scoping weirdness
-media_flow.makeClickCallback = function(data) {
-    return function() { 
-        _curFrame = 0;
-        _opts.onClick(data); 
+        },
+    
     };
-}
 
+    $.fn.media_flow = function( method ) {
+
+        // Method calling logic
+        if ( methods[method] ) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.media_flow' );
+        }    
+
+    };
+
+    $.fn.media_flow.defaults = {
+        padding : 0,
+        clipWidth : 0,
+        clipHeight : 0,
+        clipLeft : 0,
+        clipTop : 0,
+        offsetLeft : 0,
+        offsetTop : 0,
+        speed : 3,
+        rows : 0,
+        cols : 0,
+        width : 0,
+        totalWidth : 0,
+        height: 0,
+        idleFrames : 100,
+        render : function() { console.log("onRender undefined"); },
+        getData : function() { console.log("onGetData undefined"); },
+        onClick : function() { console.log("onClick undefined"); },
+        idle : function() { },
+        decorateCell : null,
+        layout : "grid",
+        compressLayout : false,
+        useTranslate3d : false,
+    }
+    
+    // internal state object
+    function flowState (opts) {
+        this.opts = opts;
+        this.started = false;
+        this.curFrame = 0;
+        this.paused = false;
+                
+        this.render = function(row) {
+            var data = this.opts.getData();
+            
+            if (data != null) {
+                html = this.opts.render(data);
+                var width = this.opts.width;
+                if (this.opts.compressLayout) {
+                    width = data.width;
+                }
+                var $cell = $("<div style='position: absolute; width: " + width + "px;'>" + html + "</div>");
+                
+                if (this.opts.decorateCell != null) {
+                    this.opts.decorateCell($cell, data);
+                }
+                
+                this.$top.append($cell);
+                
+                $cell.click((function(state, item, cell) { return function () {state.curFrame = 0; state.opts.onClick(item, cell);}}) (this, data, $cell) );
+                
+                this.positionCell($cell, row);
+                this.$div_array[row].push($cell);
+                if (!this.paused) {
+                    this.animateCell($cell, row);
+                }
+            }
+        };   
+
+        this.rowFull = function(row) {
+            if (this.opts.compressLayout) {
+                var lastX = 0;
+                if (this.$div_array[row].length > 0) {
+                    var $cell = this.$div_array[row][this.$div_array[row].length - 1];
+                    lastX = $cell.offset().left + $cell.width();
+                }
+                return lastX > this.opts.totalWidth;
+            } else {
+                return this.$div_array[row].length >= this.opts.cols;
+            }
+        };
+        
+        this.positionCell = function($cell, row) {
+            var x = 0;
+            if (this.$div_array[row].length > 0) {
+                var $lastCell = this.$div_array[row][this.$div_array[row].length - 1];
+                x = $lastCell.offset().left + $lastCell.width() - this.frame_x;
+                //console.log("position cell, num cells " + this.$div_array[row].length + " last one's left " + $lastCell.offset().left + " width " + $lastCell.width());
+            }
+            //x += this.opts.padding - this.frame_x - this.opts.offsetLeft;
+            x += this.opts.padding - this.opts.offsetLeft;
+            
+            //var x = 2 * _opts.padding + col * (_width + _opts.padding) - offset;
+            var y = this.opts.padding + row * (this.opts.height + this.opts.padding);
+            $cell.css( { "left" : x + "px", "top" : y + "px" } );
+            //console.log("set x " + x + " frame_x " + this.frame_x);
+        };
+        
+        this.animateCell = function($cell, row) {
+            //var left = $cell.offset().left + $cell.width() - this.frame_x - this.opts.offsetLeft - this.opts.padding;
+            var left = $cell.offset().left + $cell.width() - this.opts.offsetLeft - this.opts.padding - this.frame_x;
+            var time = left * 30 / this.opts.speed;
+                
+            var animSpec = {left: "-=" + left + "px", leaveTransforms : true, useTranslate3d : this.opts.useTranslate3d};
+            //console.log("animateCell left " + left);
+            var animOpt = {duration: time, 
+                           easing : "linear",
+                           complete: ((function(r) {return function() { this.animationDone(r); }}) (row)).bind(this) };
+
+           $cell.animate(animSpec, animOpt);
+        };
+        
+        this.animationDone = function(row) {
+            var $cell = this.$div_array[row].shift();
+            $cell.remove();
+            
+            this.render(row);
+            
+            this.curFrame++;
+            if (this.curFrame > this.opts.idleFrames) {
+                this.paused = true;
+                this.pauseCells();
+            }
+        };
+        
+        this.animateCells = function() {
+            for(var row = 0; row < this.opts.rows; row++) {
+                for(var col = 0; col < this.$div_array[row].length; col++) {
+                    var $cell = this.$div_array[row][col];
+                    this.animateCell($cell, row);
+                }
+            }
+        };        
+
+        this.pauseCells = function() {
+            for(var row = 0; row < this.opts.rows; row++) {
+                for(var col = 0; col < this.$div_array[row].length; col++) {
+                    var $cell = this.$div_array[row][col];
+                    $cell.clearQueue();
+                    $cell.stop();
+                }
+            }
+        };
+        
+        this.map = function(func) {
+            if (this.started) {
+                for(var row = 0; row < this.opts.rows; row++) {
+                    for(var col = 0; col < this.$div_array[row].length; col++) {
+                        func(this.$div_array[row][col]);
+                    }
+                }
+            }
+        };
+
+        this.idle = function() {
+            this.paused = true;
+            this.pauseCells();
+            this.opts.idle();
+        };
+        
+    }
+    
+})(jQuery);
